f6fd23debe11941eb7db0cd3188c09245d7894b7
ZeeshanMumtaz1989 <137243434+ZeeshanMumtaz1989@users.noreply.github.com>
2024-09-01 11:22:42 +0500

    Bugs fixed

--- pencil_sketch.py
+++ pencil_sketch.py
@@ -5,12 +5,9 @@ Description: This Python script will help you to create professional Pencil Sket
 
 Two main libraries are used which should be installed first using:
 
-pip install numpy==2.1.0               :    Fundamental package for array computing in Python
-pip install opencv-python==4.10.0.84   :    Wrapper package for OpenCV python bindings.
+pip install numpy           :    Fundamental package for array computing in Python
+pip install opencv-python   :    Wrapper package for OpenCV python bindings.
 """
-
-
-
 
 
 import cv2 
@@ -33,38 +30,8 @@ def pencil_sketch(image_path, output_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-input_image_path = 'IMG_6068.jpg'
+input_image_path = 'fazal_edited.jpg'
 output_image_path = 'Pencil_Sketch_' + input_image_path + '.jpg'
 pencil_sketch(input_image_path, output_image_path)
 
 
-
-
-
-# With more dark shade
-# import cv2 
-# import numpy as np
-
-# def pencil_sketch(image_path, output_path):
-#     image = cv2.imread(image_path)
-#     if image is None:
-#         print("Error: Could not open or find the image.")
-#         return
-
-#     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     inverted_gray_image = cv2.bitwise_not(gray_image)
-#     blurred_image = cv2.GaussianBlur(inverted_gray_image, (21, 21), 0)
-#     inverted_blurred_image = cv2.bitwise_not(blurred_image)
-    
-#     pencil_sketch_image = cv2.divide(gray_image, inverted_blurred_image, scale=256.0)
-    
-#     # Subtract a constant value to make the sketch darker
-#     pencil_sketch_image = cv2.subtract(pencil_sketch_image, np.array([30.0]))
-
-#     cv2.imwrite(output_path, pencil_sketch_image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-
-# input_image_path = 'IMG_6068.jpg'
-# output_image_path = 'Pencil_Sketch_' + input_image_path + '.jpg'
-# pencil_sketch(input_image_path, output_image_path)
